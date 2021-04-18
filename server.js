5ac9f7da0f0ed2113c2ddd4f92ee77d9239bebeb
BoraKuldeep2 <62422926+BoraKuldeep2@users.noreply.github.com>
2021-04-18 12:44:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const { ApolloServer } = require('apollo-server');
+//const { ApolloServer } = require('apollo-server');
+const express = require('express');
 const typeDefs = require('./schema');
 const casual = require('casual');
+const cors = require('cors');
+const {  ApolloServer } = require('apollo-server-express');
+const bodyParser = require('body-parser');
+const { makeExecutableSchema } = require('graphql-tools');
 
 const getUser = () => ({
   name: casual.name,
@@ -31,12 +36,33 @@ const resolvers = {
   }
 };
 
-const server = new ApolloServer({ cors: true, typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(() => {
-  console.log(`
-    Server is running!
-    Listening on port 4000
-    Explore at https://studio.apollographql.com/dev
-  `);
-});
+//const myGraphQLSchema = makeExecutableSchema({ typeDefs, resolvers })
+const app = express();
+app.use(cors());
+
+server.applyMiddleware({
+  app,
+  path: '/',
+  cors: true, // disables the apollo-server-express cors to allow the cors middleware use
+})
+
+//app.use('/', express.json(), graphqlExpress({ schema: myGraphQLSchema }));
+//app.get('/', graphiqlExpress({ endpointURL: '/' }))
+
+
+
+const port = process.env.PORT || 5000
+app.listen(port, (err) => {
+  if (err) throw err
+  console.log(`Graphql Server started on: http://localhost:${port}`)
+})
+
+// server.listen().then(() => {
+//   console.log(`
+//     Server is running!
+//     Listening on port 4000
+//     Explore at https://studio.apollographql.com/dev
+//   `);
+// });
